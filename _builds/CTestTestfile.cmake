673c76c27158c792957047c51a9d40df7fcbf9e4
Merlin13-666 <danya-a@inbox.ru>
2021-05-24 01:25:11 +0300

    first commit

---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/daniel/Merlin13-666/workspace/projects/lab-7
+# Build directory: /home/daniel/Merlin13-666/workspace/projects/lab-7/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/daniel/Merlin13-666/workspace/projects/lab-7/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/daniel/Merlin13-666/workspace/projects/lab-7/CMakeLists.txt;64;add_test;/home/daniel/Merlin13-666/workspace/projects/lab-7/CMakeLists.txt;0;")
